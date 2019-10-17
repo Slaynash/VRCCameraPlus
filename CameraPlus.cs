7d545e79627481702e22341fe2644ea42effe7c6
Herp Derpinstine <herpderpinstine@gmail.com>
2019-10-16 22:29:10 -0500

    Add files via upload

--- CameraPlus.cs
+++ CameraPlus.cs
@@ -1,108 +1,104 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 using VRCModLoader;
-using VRCSDK2;
 using VRCTools;
-using VRCTools.utils;
 
 namespace CameraPlus
 {
-    [VRCModInfo("CameraPlus", "1.0.2", "Slaynash")]
+    [VRCModInfo("CameraPlus", "2.0", "Slaynash and Herp Derpinstine")]
     public class CameraPlus : VRCMod
     {
-
-        private GameObject cameraHelper;
-
-        public void OnApplicationStart()
-        {
-            ModManager.StartCoroutine(InitEnhancedCamera());
-        }
-
-        private void CleanComponents(GameObject node)
-        {
-            GameObject.DestroyImmediate(node.GetComponent<VRC_Trigger>(), true);
-            GameObject.DestroyImmediate(node.GetComponent<VRC_EventHandler>(), true);
-        }
+        public void OnApplicationStart() { ModManager.StartCoroutine(InitEnhancedCamera()); }
 
         private IEnumerator InitEnhancedCamera()
         {
             yield return VRCUiManagerUtils.WaitForUiManagerInit();
-
-            UserCameraController userCameraController = Resources.FindObjectsOfTypeAll<UserCameraController>()[0];
-            VRCModLogger.Log("[CameraPlus] UserCameraController instance: " + userCameraController);
-            cameraHelper = userCameraController.photoCamera.transform.Find("camera_lens_mesh")?.gameObject;
-
-            //Zoom In
-            GameObject zoomInButton = GameObject.Instantiate(userCameraController.viewFinder.transform.Find("PhotoControls/Right_Filters").gameObject, userCameraController.viewFinder.transform);
-            CleanComponents(zoomInButton);
-
-            VRCT_Trigger zoomInTrigger = VRCT_Trigger.CreateVRCT_Trigger(zoomInButton, () => {
-                Camera cam1 = userCameraController.photoCamera.GetComponent<Camera>();
-                if (cam1.fieldOfView - 10 > 0) cam1.fieldOfView -= 10;
-                Camera cam2 = userCameraController.videoCamera.GetComponent<Camera>();
-                if (cam2.fieldOfView - 10 > 0) cam2.fieldOfView -= 10;
-                userCameraController.speaker.PlayOneShot(userCameraController.buttonSound);
-            });
-            zoomInTrigger.interactText = "Zoom in";
-            zoomInButton.transform.localPosition = zoomInButton.transform.localPosition + new Vector3(-0.05f, 0, 0);
-            SetButtonTexture(zoomInButton, ImageDatas.zoomin);
             
-            //Zoom Out
+            // Grab Controller
+            UserCameraController userCameraController = Resources.FindObjectsOfTypeAll<UserCameraController>()[0];
+
+            // Create Sprites
+            Sprite zoomin_sprite = CreateSprite(ImageData.zoomin_image);
+            Sprite zoomout_sprite = CreateSprite(ImageData.zoomout_image);
+            Sprite cameraindicator_on_sprite = CreateSprite(ImageData.cameraindicator_on_image);
+            Sprite cameraindicator_off_sprite = CreateSprite(ImageData.cameraindicator_off_image);
+
+            // Zoom-In
+            GameObject zoomInButton = GameObject.Instantiate(userCameraController.viewFinder.transform.Find("PhotoControls/Right_Filters").gameObject, userCameraController.viewFinder.transform);
+            VRCSDK2.VRC_CustomTrigger.Create("Zoom-In", zoomInButton, () =>
+            {
+                Camera cam = userCameraController.photoCamera.GetComponent<Camera>();
+                if ((cam.fieldOfView - 10) > 0) cam.fieldOfView -= 10;
+                cam = userCameraController.videoCamera.GetComponent<Camera>();
+                if ((cam.fieldOfView - 10) > 0) cam.fieldOfView -= 10;
+                userCameraController.speaker.PlayOneShot(userCameraController.buttonSound);
+            });
+            SetButtonSprite(zoomInButton, zoomin_sprite);
+            SetButtonIconScale(zoomInButton);
+            SetButtonOffset(zoomInButton);
+
+            // Zoom-Out
             GameObject zoomOutButton = GameObject.Instantiate(userCameraController.viewFinder.transform.Find("PhotoControls/Right_Extender").gameObject, userCameraController.viewFinder.transform);
-            CleanComponents(zoomOutButton);
-            VRCT_Trigger zoomOutTrigger = VRCT_Trigger.CreateVRCT_Trigger(zoomOutButton, () => {
-                Camera cam1 = userCameraController.photoCamera.GetComponent<Camera>();
-                if (cam1.fieldOfView + 10 < 180) cam1.fieldOfView += 10;
-                Camera cam2 = userCameraController.videoCamera.GetComponent<Camera>();
-                if (cam2.fieldOfView + 10 < 180) cam2.fieldOfView += 10;
-                userCameraController.speaker.PlayOneShot(userCameraController.buttonSound);
-            });
-            zoomOutTrigger.interactText = "Zoom out";
-            zoomOutButton.transform.localPosition = zoomOutButton.transform.localPosition + new Vector3(-0.05f, 0, 0);
-            SetButtonTexture(zoomOutButton, ImageDatas.zoomout);
-
-            //Toggle camera helper
-            GameObject toggleCameraHelperButton = GameObject.Instantiate(userCameraController.viewFinder.transform.Find("PhotoControls/Right_Timer").gameObject, userCameraController.viewFinder.transform);
-            CleanComponents(toggleCameraHelperButton);
-            VRCT_Trigger toggleCameraHelperTrigger = VRCT_Trigger.CreateVRCT_Trigger(toggleCameraHelperButton, () => {
-                cameraHelper?.SetActive(!cameraHelper.activeSelf);
-                userCameraController.speaker.PlayOneShot(userCameraController.buttonSound);
-            });
-            toggleCameraHelperTrigger.interactText = "Toggle camera indicator";
-            toggleCameraHelperButton.transform.localPosition = toggleCameraHelperButton.transform.localPosition + new Vector3(-0.05f, 0, 0);
-
-            //Scale body to make it looks better
-            Transform body = userCameraController.viewFinder.transform.Find("camera_mesh/body");
-            body.localPosition = body.localPosition + new Vector3(-0.025f, 0, 0);
-            body.localScale = body.localScale + new Vector3(0.8f, 0, 0);
-            foreach (Transform trans in toggleCameraHelperButton.transform)
+            VRCSDK2.VRC_CustomTrigger.Create("Zoom-Out", zoomOutButton, () =>
             {
-                if (trans != null && trans.gameObject.name.StartsWith("icon-"))
-                    trans.gameObject.SetActive(false);
-            }
+                Camera cam = userCameraController.photoCamera.GetComponent<Camera>();
+                if ((cam.fieldOfView + 10) < 180) cam.fieldOfView += 10;
+                cam = userCameraController.videoCamera.GetComponent<Camera>();
+                if ((cam.fieldOfView + 10) < 180) cam.fieldOfView += 10;
+                userCameraController.speaker.PlayOneShot(userCameraController.buttonSound);
+            });
+            SetButtonSprite(zoomOutButton, zoomout_sprite);
+            SetButtonIconScale(zoomOutButton);
+            SetButtonOffset(zoomOutButton);
+
+            // Toggle Camera Indicator
+            GameObject cameraHelper = userCameraController.photoCamera.transform.Find("camera_lens_mesh").gameObject;
+            GameObject toggleCameraIndicatorButton = GameObject.Instantiate(userCameraController.viewFinder.transform.Find("PhotoControls/Right_Timer").gameObject, userCameraController.viewFinder.transform);
+            VRCSDK2.VRC_CustomTrigger.Create("Camera Indicator", toggleCameraIndicatorButton, () =>
+            {
+                cameraHelper.SetActive(!cameraHelper.activeSelf);
+                if (cameraHelper.activeSelf)
+                    SetButtonSprite(toggleCameraIndicatorButton, cameraindicator_on_sprite);
+                else
+                    SetButtonSprite(toggleCameraIndicatorButton, cameraindicator_off_sprite);
+                userCameraController.speaker.PlayOneShot(userCameraController.buttonSound);
+            });
+            SetButtonSprite(toggleCameraIndicatorButton, cameraindicator_on_sprite);
+            SetButtonIconScale(toggleCameraIndicatorButton);
+            SetButtonOffset(toggleCameraIndicatorButton);
+
+            // Resize Camera Body
+            Transform camera_body = userCameraController.viewFinder.transform.Find("camera_mesh/body");
+            camera_body.localPosition = camera_body.localPosition + new Vector3(-0.025f, 0, 0);
+            camera_body.localScale = camera_body.localScale + new Vector3(0.8f, 0, 0);
         }
 
-        private void SetButtonTexture(GameObject button, string base64texture)
+        private static Sprite CreateSprite(string base64texture)
         {
-            VRCModLogger.Log("[CameraPlus] Setting button texture");
-            Texture2D t = new Texture2D(2, 2);
-            Texture2DUtils.LoadImage(t, Convert.FromBase64String(base64texture));
-            foreach(Transform trans in button.transform)
-            {
-                if (trans == null) continue;
-                if (trans.gameObject.name.StartsWith("icon"))
-                {
-                    VRCModLogger.Log("[CameraPlus] Found icon element " + trans.gameObject);
-                    trans.GetComponent<SpriteRenderer>().sprite = Sprite.Create(t, new Rect(0.0f, 0.0f, t.width, t.height), new Vector2(0.5f, 0.5f), 100.0f);
-                    trans.localScale /= 2.4f;
-                    VRCModLogger.Log("[CameraPlus] Icon updated successfully");
-                }
-            }
-            VRCModLogger.Log("[CameraPlus] Done setting button texture");
+            Texture2D texture = new Texture2D(2, 2);
+            VRCTools.utils.Texture2DUtils.LoadImage(texture, Convert.FromBase64String(base64texture));
+            return Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f);
         }
+
+        private static void SetButtonIconScale(GameObject button)
+        {
+            foreach (Transform trans in button.transform)
+            {
+                if ((trans != null) && trans.gameObject.name.StartsWith("icon"))
+                    trans.localScale /= 2.4f;
+            }
+        }
+
+        private static void SetButtonSprite(GameObject button, Sprite sp)
+        {
+            foreach (Transform trans in button.transform)
+            {
+                if ((trans != null) && trans.gameObject.name.StartsWith("icon"))
+                    trans.GetComponent<SpriteRenderer>().sprite = sp;
+            }
+        }
+
+        private static void SetButtonOffset(GameObject button) { button.transform.localPosition = button.transform.localPosition + new Vector3(-0.05f, 0, 0); }
     }
 }
